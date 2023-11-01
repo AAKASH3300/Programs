9bdcb33bc63b1173afb328e6fdb6bb3226651969
Aakash <0412.aakash@gmail.com>
2023-11-01 16:31:31 +0530

    firstcommit

---
+++ Day_1/test.java
@@ -0,0 +1,20 @@
+package Day_1;
+
+public class test {
+    // instance variable
+    int instanceVar;
+
+    public static void main(String[] args) {
+        // local variable
+        int localVar = 10;
+
+        // static variable
+        staticVar = 20;
+
+        System.out.println("Local variable: " + localVar);
+        System.out.println("Static variable: " + staticVar);
+    }
+
+    // static variable
+    static int staticVar;
+}
